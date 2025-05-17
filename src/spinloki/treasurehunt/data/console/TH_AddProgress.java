c02f73201880b02f719bfe7ef610ac47d4b3b1d5
spinloki <spinloki997@gmail.com>
2025-05-17 13:55:47 -0600

    Added console command for treasure hunt progress gain

---
+++ src/spinloki/treasurehunt/data/console/TH_AddProgress.java
@@ -0,0 +1,34 @@
+package spinloki.treasurehunt.data.console;
+
+import org.jetbrains.annotations.NotNull;
+import org.lazywizard.console.BaseCommand;
+import org.lazywizard.console.CommonStrings;
+import org.lazywizard.console.Console;
+import spinloki.treasurehunt.campaign.intel.events.THTimeFactor;
+import spinloki.treasurehunt.campaign.intel.events.TreasureHuntEventIntel;
+
+import static org.lazywizard.console.CommandUtils.isInteger;
+
+public class TH_AddProgress implements BaseCommand {
+    @Override
+    public CommandResult runCommand(@NotNull String args, @NotNull CommandContext context) {
+        if (!context.isInCampaign())
+        {
+            Console.showMessage(CommonStrings.ERROR_CAMPAIGN_ONLY);
+            return CommandResult.WRONG_CONTEXT;
+        }
+
+        if (!isInteger(args)){
+            return CommandResult.BAD_SYNTAX;
+        }
+
+        var amount = Integer.parseInt(args);
+        if (amount < 0){
+            return CommandResult.BAD_SYNTAX;
+        }
+
+        TreasureHuntEventIntel.addFactorCreateIfNecessary(new THTimeFactor(amount), null);
+        Console.showMessage("Created factor to add progress");
+        return CommandResult.SUCCESS;
+    }
+}
